a2857f102a459c74b3a4d9ac1f2963227ce0b675
brendanjgibbons <47793782+brendanjgibbons@users.noreply.github.com>
2021-07-27 19:50:52 -0400

    Add files via upload

---
+++ slots.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "baseclass.h"
+#include "slots.h"
+
+int main()
+{
+	Player player1(0, "Brendan");
+
+	gameplay(player1);
+}
