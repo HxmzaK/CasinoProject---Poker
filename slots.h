10b251b823257a4eb91d72d86933c3baf59893b3
brendanjgibbons <47793782+brendanjgibbons@users.noreply.github.com>
2021-07-27 22:07:42 -0400

    Add files via upload

--- slots.h
+++ slots.h
@@ -6,78 +6,75 @@
 
 using namespace std;
 
-double singleRow();
-double doubleRow();
-double checkForMatches(string s1, string s2, string s3);
 
-
-int gameplay(Player player1)
+	double checkForMatches(string spin1, string spin2, string spin3, Game targetGame, Player targetPlayer)
 	{
+		double award = 0;
+		string symbols[5] = { "Cherries", "Bar", "Oranges", "Melons", "Plums" };
 
-		int choice = 0, repeat = 1, catchEnter = 0;;
+		int lossCount = 0;
 
-		int balance = Player::GetBalancedb();
+		cout << "\nChecking for matches...\n\n";
 
-		while (repeat == 1)
+		bool matching = false;
+
+		if (spin1 == symbols[0] && spin2 == symbols[0] && spin3 == symbols[0])
 		{
-			cout << "Welcome to Slots!\n\nMenu:\n1. Single row\n2. Double row\n3. Player statistics\n4. How to play\n5. Exit\n\nMake a selection: ";
-			cin >> choice;
+			cout << "Winner! You spun three cherries. Your award is $250" << endl;
+			award += 250;
+			targetGame.AddLoss();
+			targetPlayer.AddWinPlayer();
+			
+		}
+		else if (spin1 == symbols[1] && spin2 == symbols[1] && spin3 == symbols[1])
+		{
+			cout << "Winner! You spun three bars. Your award is $5000" << endl;
+			award += 5000;
+			targetGame.AddLoss();
+			targetPlayer.AddWinPlayer();
 
-			switch (choice)
-			{
-			case 1:
+		}
+		else if (spin1 == symbols[2] && spin2 == symbols[2] && spin3 == symbols[2])
+		{
+			cout << "Winner! You spun three oranges. Your award is $500" << endl;
+			award += 500; 
+			targetGame.AddLoss();
+			targetPlayer.AddWinPlayer();
 
-				winnings += singleRow();
-				Player::UpdateBalance(int(balance + winnings));
+		}
+		else if (spin1 == symbols[3] && spin2 == symbols[3] && spin3 == symbols[3])
+		{
+			cout << "Winner! You spun three melons. Your award is $750" << endl;
+			award += 750;
+			targetGame.AddLoss();
+			targetPlayer.AddWinPlayer();
 
-				break;
-			case 2:
+		}
+		else if (spin1 == symbols[4] && spin2 == symbols[4] && spin3 == symbols[4])
+		{
+			cout << "Winner! You spun three plums. Your award is $300" << endl;
+			award += 300;
+			targetGame.AddLoss();
+			targetPlayer.AddWinPlayer();
 
-				winnings += doubleRow();
-				Player::UpdateBalance(int(balance + winnings));
+		}
+		else
+		{
+			cout << "No matching symbols were spun, no award given.\n";
+			lossCount++;
+			targetGame.AddWin();
+			targetPlayer.AddLossPlayer();
 
-				break;
-			case 3:
-
-				cout << "Player Statistics:\n\nWins: " << Player::GetWinsdb() << "\nLosses: " << Player::GetLossesdb() << "\nGames Played: " << Player::GetGamesPlayeddb() << "\nBalance: " << Player::GetBalancedb() << "\n\n==================\n" << endl;
-
-				break;
-			case 4:
-
-				cout << "\n===== SLOTS =====\n\nIn the game of slots, the goal of the game is to pull the\nlever, which will roll random numbers. If these numbers\nline up with a specific combination, an award will be given,\ndepending on what the combination is.\n\n";
-
-				break;
-
-			case 5:
-				exit(0);
-				break;
-
-			default:
-				break;
-
-			}
-
-			cout << endl;
 		}
 
-		cout << "See you later! Thanks for playing slots!\n\n";
+		targetGame.AddAmountLost(award);
+		targetGame.AddAmountwon(lossCount * 5);
 
-
-		// Updates database as game exits
-		Game::UpdateAmountLost();
-		Game::UpdateAmountWon();
-		Game::UpdateGamesplayed();
-		Game::UpdateLosses();
-		Game::UpdateWins();
-		
-		Player::UpdateWins();
-		Player::UpdateLosses();
-		Player::UpdateGamesplayed();
-
-		return 0;
+		return award;
 	}
 
-	double singleRow()
+
+	double singleRow(Game targetGame, Player targetPlayer)
 	{
 		string symbols[5] = { "Cherries", "Bar", "Oranges", "Melons", "Plums" };
 		string spins[3];
@@ -99,24 +96,24 @@ int gameplay(Player player1)
 
 			}
 
-			award += checkForMatches(spins[0], spins[1], spins[2]);
+			award += checkForMatches(spins[0], spins[1], spins[2], targetGame, targetPlayer);
 
-			Player::AddGameplayedPlayer();
-			Game::AddGameplayed();
-
+			targetPlayer.AddGameplayedPlayer();
+			targetGame.AddGameplayed();
+			
 			cout << "Continue playing? (1 for yes, 0 for no): ";
 			cin >> repeat;
 
 		}
 
-
+		award = award - (playcount * 5);
 		cout << "\nYou have earned $" << award << " while playing double row slots. Exiting to main menu...\n\n";
 		return award;
 
 	}
 
 
-	double doubleRow()
+	double doubleRow(Game targetGame, Player targetPlayer)
 	{
 		string symbols[5] = { "Cherries", "Bar", "Oranges", "Melons", "Plums" };
 		string spins[3][2];
@@ -143,86 +140,92 @@ int gameplay(Player player1)
 
 			}
 
-			award += checkForMatches(spins[0][0], spins[0][1], spins[0][2]);
-			award += checkForMatches(spins[1][0], spins[1][1], spins[1][2]);
+			award += checkForMatches(spins[0][0], spins[0][1], spins[0][2], targetGame, targetPlayer);
+			award += checkForMatches(spins[1][0], spins[1][1], spins[1][2], targetGame, targetPlayer);
 
-			Player::AddGameplayedPlayer();
-			Game::AddGameplayed();
+			targetPlayer.AddGameplayedPlayer();
+			targetGame.AddGameplayed();
 
 			cout << "Continue playing? (1 for yes, 0 for no): ";
 			cin >> repeat;
 
 		}
 
+		award = award - (playcount * 5);
 		cout << "\nYou have earned $" << award << " while playing double row slots. Exiting to main menu...\n\n";
 		return award;
 
 
 	}
 
+	
 
-	double checkForMatches(string spin1, string spin2, string spin3)
+
+int gameplay(Player player1)
 	{
-		double award = 0;
-		string symbols[5] = { "Cherries", "Bar", "Oranges", "Melons", "Plums" };
+		Game Slots(5);
+		int choice = 0, repeat = 1, catchEnter = 0;;
+		float winnings;
 
-		lossCount = 0;
+		int balance = player1.GetBalancedb();
 
-		cout << "\nChecking for matches...\n\n";
-
-		bool matching = false;
-
-		if (spin1 == symbols[0] && spin2 == symbols[0] && spin3 == symbols[0])
+		while (repeat == 1)
 		{
-			cout << "Winner! You spun three cherries. Your award is $250" << endl;
-			award += 250;
-			Game::AddLoss();
-			Player::AddWinPlayer();
-			
-		}
-		else if (spin1 == symbols[1] && spin2 == symbols[1] && spin3 == symbols[1])
-		{
-			cout << "Winner! You spun three bars. Your award is $5000" << endl;
-			award += 5000;
-			Game::AddLoss();
-			Player::AddWinPlayer();
+			cout << "Welcome to Slots!\n\nMenu:\n1. Single row\n2. Double row\n3. Player statistics\n4. How to play\n5. Exit\n\nMake a selection: ";
+			cin >> choice;
 
-		}
-		else if (spin1 == symbols[2] && spin2 == symbols[2] && spin3 == symbols[2])
-		{
-			cout << "Winner! You spun three oranges. Your award is $500" << endl;
-			award += 500; 
-			Game::AddLoss();
-			Player::AddWinPlayer();
+			switch (choice)
+			{
+			case 1:
 
-		}
-		else if (spin1 == symbols[3] && spin2 == symbols[3] && spin3 == symbols[3])
-		{
-			cout << "Winner! You spun three melons. Your award is $750" << endl;
-			award += 750;
-			Game::AddLoss();
-			Player::AddWinPlayer();
+				winnings += singleRow(Slots, player1);
+				player1.UpdateBalance(int(balance + winnings));
 
-		}
-		else if (spin1 == symbols[4] && spin2 == symbols[4] && spin3 == symbols[4])
-		{
-			cout << "Winner! You spun three plums. Your award is $300" << endl;
-			award += 300;
-			Game::AddLoss();
-			Player::AddWinPlayer();
+				break;
+			case 2:
 
-		}
-		else
-		{
-			cout << "No matching symbols were spun, no award given.\n";
-			losscount++;
-			Game::AddWin();
-			Player::AddLossPlayer();
+				winnings += doubleRow(Slots, player1);
+				player1.UpdateBalance(int(balance + winnings));
 
+				break;
+			case 3:
+
+				cout << "Player Statistics:\n\nWins: " << player1.GetWinsdb() << "\nLosses: " << player1.GetLossesdb() << "\nGames Played: " << player1.GetGamesPlayeddb() << "\nBalance: " << player1.GetBalancedb() << "\n\n==================\n" << endl;
+
+				break;
+			case 4:
+
+				cout << "\n===== SLOTS =====\n\nIn the game of slots, the goal of the game is to pull the\nlever, which will roll random numbers. If these numbers\nline up with a specific combination, an award will be given,\ndepending on what the combination is.\n\n";
+
+				break;
+
+			case 5:
+				exit(0);
+				break;
+
+			default:
+				break;
+
+			}
+
+			cout << endl;
 		}
 
-		Game::AddAmountLost(award);
-		Game::AddAmountWon(lossCount * 5);
+		cout << "See you later! Thanks for playing slots!\n\n";
 
-		return award;
+
+		// Updates database as game exits
+		Slots.UpdateAmountLost();
+		Slots.UpdateAmountwon();
+		Slots.UpdateGamesplayed();
+		Slots.UpdateLosses();
+		Slots.UpdateWins();
+		
+		player1.UpdateWins();
+		player1.UpdateLosses();
+		player1.UpdateGamesplayed();
+
+		return 0;
 	}
+
+	
