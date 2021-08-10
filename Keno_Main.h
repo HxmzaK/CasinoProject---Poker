ffc50288ea05303a926af5c0cf76bc886a71511e
JulianFontes <74979962+JulianFontes@users.noreply.github.com>
2021-08-10 16:54:16 -0400

    Minor update to Keno_Main.h

--- Keno_Main.h
+++ Keno_Main.h
@@ -50,30 +50,30 @@ int Keno(Player player1){
     //Warm Welcome :)
 
     //Linux Terminal Print:
-    printf("\033[0;33m"); //print yellow text to terminal
-    std::cout << 
-    
-    "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗     ██╗  ██╗███████╗███╗   ██╗ ██████╗ \n"
-    "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗    ██║ ██╔╝██╔════╝████╗  ██║██╔═══██╗\n"
-    "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║    █████╔╝ █████╗  ██╔██╗ ██║██║   ██║\n"
-    "██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║    ██╔═██╗ ██╔══╝  ██║╚██╗██║██║   ██║\n"
-    "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝    ██║  ██╗███████╗██║ ╚████║╚██████╔╝\n"
-    " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ \n"
-    "                                                                                                                           " 
-    << std::endl;
-    
-    // //Windows Terminal Print:
     // printf("\033[0;33m"); //print yellow text to terminal
-    // std::cout <<
-
-    //     "____    __    ____  _______  __        ______   ______   .___  ___.  _______    .___________.  ______       __  ___  _______ .__   __.   ______    __  \n"
-    //     "\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|   |           | /  __  \\     |  |/  / |   ____||  \\ |  |  /  __  \\  |  | \n"
-    //     " \\   \\/    \\/   /  |  |__   |  |     |  ,----'|  |  |  | |  \\  /  | |  |__      `---|  |----`|  |  |  |    |  '  /  |  |__   |   \\|  | |  |  |  | |  | \n"
-    //     "  \\            /   |   __|  |  |     |  |     |  |  |  | |  |\\/|  | |   __|         |  |     |  |  |  |    |    <   |   __|  |  . `  | |  |  |  | |  | \n"
-    //     "   \\    /\\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____        |  |     |  `--'  |    |  .  \\  |  |____ |  |\\   | |  `--'  | |__| \n"
-    //     "    \\__/  \\__/     |_______||_______| \\______| \\______/  |__|  |__| |_______|       |__|      \\______/     |__|\\__\\ |_______||__| \\__|  \\______/  (__) \n"
-    //     "                                                                                                                                                       "
+    // std::cout << 
+    
+    // "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗     ██╗  ██╗███████╗███╗   ██╗ ██████╗ \n"
+    // "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗    ██║ ██╔╝██╔════╝████╗  ██║██╔═══██╗\n"
+    // "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║    █████╔╝ █████╗  ██╔██╗ ██║██║   ██║\n"
+    // "██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║    ██╔═██╗ ██╔══╝  ██║╚██╗██║██║   ██║\n"
+    // "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝    ██║  ██╗███████╗██║ ╚████║╚██████╔╝\n"
+    // " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ \n"
+    // "                                                                                                                           " 
     // << std::endl;
+    
+    //Windows Terminal Print:
+    printf("\033[0;33m"); //print yellow text to terminal
+    std::cout <<
+
+        "____    __    ____  _______  __        ______   ______   .___  ___.  _______    .___________.  ______       __  ___  _______ .__   __.   ______    __  \n"
+        "\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|   |           | /  __  \\     |  |/  / |   ____||  \\ |  |  /  __  \\  |  | \n"
+        " \\   \\/    \\/   /  |  |__   |  |     |  ,----'|  |  |  | |  \\  /  | |  |__      `---|  |----`|  |  |  |    |  '  /  |  |__   |   \\|  | |  |  |  | |  | \n"
+        "  \\            /   |   __|  |  |     |  |     |  |  |  | |  |\\/|  | |   __|         |  |     |  |  |  |    |    <   |   __|  |  . `  | |  |  |  | |  | \n"
+        "   \\    /\\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____        |  |     |  `--'  |    |  .  \\  |  |____ |  |\\   | |  `--'  | |__| \n"
+        "    \\__/  \\__/     |_______||_______| \\______| \\______/  |__|  |__| |_______|       |__|      \\______/     |__|\\__\\ |_______||__| \\__|  \\______/  (__) \n"
+        "                                                                                                                                                       "
+    << std::endl;
 
     std::cout << "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------- \n";
     std::cout << "RULES: To play Keno select the desired number of spots ranging from 1-10, and the wager amount for each drawing from $1-10 as well. \n";
@@ -162,7 +162,7 @@ int Keno(Player player1){
             {
                 //Print player Record: Games Played, Wins to Losses, and Amount Won/Lost (during session)
                 std::cout << "------------------------------------------------ \n";
-                std::cout << "Record: \nGames Played: " << player1.getGamePlayed() << "\nRecord: " << player1.getWins() << " - " << player1.getLosses() << "\nAmount Won in Session: " << player1.getAmountWon() << "\nAmount Lost in Session: " << player1.getAmountLost() << std::endl;
+                std::cout << "Record: \nGames Played: " << player1.GetGamesPlayeddb() << "\nRecord: " << player1.GetWinsdb() << " - " << player1.GetLossesdb() << "\nAmount Won in Session: " << player1.getAmountWon() << "\nAmount Lost in Session: " << player1.getAmountLost() << std::endl;
                 std::cout << "------------------------------------------------- \n";
 
                 //Print Player Account: Amount Withdrawn, Account Balance, Spending Amount
@@ -274,8 +274,8 @@ int Keno(Player player1){
                         moneyWon = prizeCalculator(ticket1, matches, wagerAmount); //assign prize value to amount of money won
 
                         //add to gamesPlayed and update gamesPlayed
-                        int addGames = player1.getGamePlayed() + 1; //store new value of Games Played for PLAYERS db table
-                        int addGames_Games = Keno.getGamesPlayed() + 1; //store new value of Games Played for GAMES db table
+                        int addGames = player1.GetGamesPlayeddb() + 1; //store new value of Games Played for PLAYERS db table
+                        int addGames_Games = Keno.GetGamesPlayeddb() + 1; //store new value of Games Played for GAMES db table
                         player1.setGamesPlayed(addGames);
                         string uptGamesPlayed_Games = "UPDATE GAMES SET GAMESPLAYED = "+ std::to_string(addGames_Games) + " WHERE ID = "+ std::to_string(Keno.getID()) + ";"; //update gamesplayed GAMES db
                         string uptGamesPlayed_Players = "UPDATE PLAYERS SET GAMESPLAYED = "+ std::to_string(addGames) + " WHERE ID = "+ std::to_string(player1.getID()) + ";"; //update gameplayed PLAYERS db
@@ -537,8 +537,8 @@ int Keno(Player player1){
                         float moneyWon = prizeCalculator(ticket1, matches1, wagerAmount1) + prizeCalculator(ticket1, matches2, wagerAmount2); //set moneyWon to sum of prizes from both tickets
 
                         //add to gamesPlayed and update gamesPlayed
-                        int addGames = player1.getGamePlayed() + 1;
-                        int addGames_Games = Keno.getGamesPlayed() + 1;
+                        int addGames = player1.GetGamesPlayeddb() + 1;
+                        int addGames_Games = Keno.GetGamesPlayeddb() + 1;
                         player1.setGamesPlayed(addGames);
                         string uptGamesPlayed_Games = "UPDATE GAMES SET GAMESPLAYED = "+ std::to_string(addGames_Games) + " WHERE ID = "+ std::to_string(Keno.getID()) + ";";
                         string uptGamesPlayed_Players = "UPDATE PLAYERS SET GAMESPLAYED = "+ std::to_string(addGames) + " WHERE ID = "+ std::to_string(player1.getID()) + ";";
@@ -759,8 +759,8 @@ int Keno(Player player1){
                     }
 
                         //add to gamesPlayed and update gamesPlayed
-                        int addGames = player1.getGamePlayed() + 1;
-                        int addGames_Games = Keno.getGamesPlayed() + 1;
+                        int addGames = player1.GetGamesPlayeddb() + 1;
+                        int addGames_Games = Keno.GetGamesPlayeddb() + 1;
                         player1.setGamesPlayed(addGames);
                         string uptGamesPlayed_Games = "UPDATE GAMES SET GAMESPLAYED = "+ std::to_string(addGames_Games) + " WHERE ID = "+ std::to_string(Keno.getID()) + ";";
                         string uptGamesPlayed_Players = "UPDATE PLAYERS SET GAMESPLAYED = "+ std::to_string(addGames) + " WHERE ID = "+ std::to_string(player1.getID()) + ";";
